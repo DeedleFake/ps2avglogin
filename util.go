dc42c25aa7bf9fc2136277b740848b29891a625c
DeedleFake <deedlefake@users.noreply.github.com>
2016-04-10 20:43:40 -0400

    Add godoc comment to walkStruct().

--- util.go
+++ util.go
@@ -4,6 +4,11 @@ import (
 	"reflect"
 )
 
+// walkStruct walks through a struct and all sub-structs recustively,
+// calling f on all 'leaf' fields. The first argument to f is the name
+// of the field in the form parent.field, and the second argument is
+// the field itself. If any call to f returns non-nil error,
+// walkStruct stops and returns that error.
 func walkStruct(s interface{}, f func(string, reflect.Value) error) error {
 	var inner func(v reflect.Value, p string) error
 	inner = func(v reflect.Value, p string) error {
